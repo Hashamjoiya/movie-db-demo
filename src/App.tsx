c93eafab5f893b52854906410c7f065b745dfe10
Hasham Asif <hashamjoiya@gmail.com>
2022-06-20 02:43:08 +0500

    updated main view UI

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,8 @@ import './App.scss';
 
 function App() {
   return (
-    <div className="bg-slate-800 text-white App">
+    <div className="bg-slate-800 text-white App flex flex-col">
+      <h1 className='text-3xl text-center p-4 font-semibold'>Movie <span className='text-indigo-400'>DB</span> Demo</h1>
       <Movies />
     </div>
   );
--- src/features/movies/Movies.tsx
+++ src/features/movies/Movies.tsx
@@ -1,18 +1,26 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useAppDispatch, useAppSelector } from '../../app/hooks';
 import { fetchMovies, moviesState } from './movieSlice';
+import './Movies.scss';
 
 export function Movies() {
     const movies = useAppSelector(moviesState);
     const dispatch = useAppDispatch();
+    const posterBasePath = 'https://image.tmdb.org/t/p/w500'
+
+    useEffect(() => {
+        dispatch(fetchMovies({ page: 1 }))
+    }, [])
+
     return (
-        <div>
-            <div>Hi</div>
-            <button onClick={() => dispatch(fetchMovies({ page: 1 }))}>Fetch</button>
+        <div className='md:px-12 xl:px-24 py-8 h-full overflow-y-auto movies-container'>
             {movies.results?.length > 0 &&
-                <div className="grid grid-cols-4 gap-4">
+                <div className="w-2/3 xl:w-3/4 mx-auto grid grid-cols-1 md:grid-cols-3 xl:grid-cols-4 gap-16 justify-center items-center">
                     {movies.results.map(movie =>
-                        <div>{movie.title}</div>
+                        <div key={movie.id} className="mx-auto rounded-md h-64 w-44 bg-slate-400 duration-500 shadow-slate-700 shadow-md cursor-pointer hover:shadow-lg hover:h-72 hover:w-48 flex flex-col movies-card">
+                            <img className='poster h-4/5' src={posterBasePath + movie.poster_path}></img>
+                            <div className='p-px h-1/5 flex flex-row justify-center items-center text-center'>{movie.title}</div>
+                        </div>
                     )}
                 </div>
             }
