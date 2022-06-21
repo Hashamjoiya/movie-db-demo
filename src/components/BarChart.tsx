dcc399485d90f868d36dc304eee28c4ab45faeaa
Hasham Asif <hashamjoiya@gmail.com>
2022-06-21 16:29:38 +0500

    basic bar chart added

--- src/components/BarChart.tsx
+++ src/components/BarChart.tsx
@@ -1,24 +1,76 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import * as d3 from 'd3';
 
+const data = [
+    { country: 'United States', value: 12394 },
+    { country: 'Russia', value: 6148 },
+    { country: 'Germany (FRG)', value: 1653 },
+    { country: 'France', value: 2162 },
+    { country: 'United Kingdom', value: 1214 },
+    { country: 'China', value: 1131 },
+    { country: 'Spain', value: 814 },
+    { country: 'Netherlands', value: 1167 },
+    { country: 'Italy', value: 660 },
+    { country: 'Israel', value: 1263 }
+]
 
-export function BarChart() {
+export function BarChart(props: { data: any }) {
+    let svg: d3.Selection<SVGGElement, unknown, HTMLElement, any>
+
+    useEffect(() => {
+        if (!svg) {
+            // set the dimensions and margins of the graph
+            const margin = { top: 30, right: 30, bottom: 70, left: 60 },
+                width = 390 - margin.left - margin.right,
+                height = 400 - margin.top - margin.bottom;
+
+            // append the svg object to the body of the page
+            svg = d3.select("#bar-chart-1")
+                .append("svg")
+                .attr("width", width + margin.left + margin.right)
+                .attr("height", height + margin.top + margin.bottom)
+                .append("g")
+                .attr("transform",
+                    "translate(" + margin.left + "," + margin.top + ")");
+
+
+            // X axis
+            const x = d3.scaleBand()
+                .range([0, width])
+                .domain((props.data).map(function (d: any) { return d.title }))
+                .padding(0.2);
+            svg.append("g")
+                .attr("transform", "translate(0," + height + ")")
+                .call(d3.axisBottom(x))
+                .selectAll("text")
+                .attr("transform", "translate(-10,0)rotate(-45)")
+                .style("text-anchor", "end");
+
+            // Add Y axis
+            const y = d3.scaleLinear()
+                .domain([0, 10])
+                .range([height, 0]);
+            svg.append("g")
+                .call(d3.axisLeft(y));
+
+            // Bars
+            svg.selectAll("mybar")
+                .data(props.data)
+                .enter()
+                .append("rect")
+                .attr("x", (function (d: any) { return x(d.title); } as any))
+                .attr("y", function (d: any) { return y(d.rating); })
+                .attr("width", x.bandwidth())
+                .attr("height", function (d: any) { return height - y(d.rating); })
+                .attr("fill", "#69b3a2");
+        }
+    }, [])
+
 
     return (
 
-        <svg
-            style={{
-                height: 500,
-                width: "100%",
-                marginRight: "0px",
-                marginLeft: "0px",
-            }}
-        >
-            <g className="plot-area" />
-            <g className="x-axis" />
-            <g className="y-axis" />
-        </svg>
+        <div className='mx-auto' id="bar-chart-1"></div>
 
     );
 
--- src/components/MovieStats.tsx
+++ src/components/MovieStats.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useAppSelector } from "../app/hooks";
 import { moviesState } from "../features/movies/movieSlice";
+import { BarChart } from "./BarChart";
 
 import './MovieStats.scss'
 
@@ -12,12 +13,21 @@ export function MovieStats() {
         setTopTenMovies(movies.results?.slice(0, 10))
     }, [movies])
 
+    const parsedData = (moviesData: any[]) => {
+        return moviesData.map(e => ({
+            id: e.id,
+            title: e.original_title,
+            rating: e.vote_average,
+            votes: e.vote_count
+        }))
+    }
+
     return (
-        <div className="md:px-4 lg:px-8 xl:px-12">
+        <div className="md:px-4 lg:px-8 xl:px-12 flex flex-col items-center">
             {topTenMovies?.length < 1 && <h1>Nothing to Show</h1>}
             {topTenMovies?.length > 0 &&
                 <div>
-                    HI
+                    <BarChart data={parsedData(topTenMovies)} />
                 </div>
             }
         </div>
